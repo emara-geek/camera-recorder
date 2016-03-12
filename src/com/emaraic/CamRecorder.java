a94de70b524ab5da30492e105f51e10fd08f9db4
Emaraic <>
2016-03-12 12:22:44 +0200

    initiate project

---
+++ src/com/emaraic/CamRecorder.java
@@ -0,0 +1,15 @@
+
+package com.emaraic;
+
+/**
+ *
+ * @author Taha Emara
+ * Website http://www.emaraic.com 
+ */
+public class CamRecorder {
+
+    public static void main(String[] args) {
+       
+    }
+    
+}
